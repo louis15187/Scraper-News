442d9f76600d3bd52986f75a2b78b60dd0b3fc54
Louis Malone <lmalone874@gmail.com>
2019-10-16 16:57:02 -0400

    everything should work

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 //dependencies
-var bodyparser = require("body-parser");
+var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var logger = require("morgan");
 
@@ -9,43 +9,38 @@ var app = express();
 
 app.use(logger("dev"));
 app.use(
-    bodyparser.urlencoded({
-        extended: false
-    })
+  bodyParser.urlencoded({
+    extended: false
+  })
 );
 
-
-
+app.use(express.static(process.cwd() + "/public"));
 //Require set up handlebars
 var exphbs = require("express-handlebars");
 app.engine(
-    "handlebars",
-    exphbs({
-        defaultLayout: "main"
-    })
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
 );
 app.set("view engine", "handlebars");
 
-
+//connecting to MongoDB
+// mongoose.connect("mongodb://localhost/scraped_news");
 const MONGODB_URI =
-    process.env.MONGODB_URI || "mongodb://localhost/scraper_news";
+  process.env.MONGODB_URI || "mongodb://localhost/scraper_news";
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 var db = mongoose.connection;
-
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function() {
-    console.log("Connected to Mongoose!");
+  console.log("Connected to Mongoose!");
 });
 
-// Make public a static folder
-app.use(express.static("public"));
-
-var routes = require("./controller/controller.js");
-console.log(routes);
-app.use(routes);
-
-var port = process.env.PORT || 3000;
+var routes = require("./controller/controller");
+app.use("/", routes);
+//Create localhost port
+var port = process.env.PORT || 3002;
 app.listen(port, function() {
-    console.log("Listening on PORT" + port);
+  console.log("Listening on PORT " + port);
 });
